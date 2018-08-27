b035f3bb5645cd9dd271508eb24619e76a97912b
cr0wg4n <dark.hunter.crow@gmail.com>
2018-08-27 16:03:39 -0400

    servicio funcionando

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,17 @@
-import { 
-  BrowserModule } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { LayoutModule } from "./layout/layout.module";
 import { AppRoutingModule } from "./app-routing.module";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,LayoutModule,AppRoutingModule,BrowserAnimationsModule
+    BrowserModule,HttpClientModule,LayoutModule,AppRoutingModule,BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
